e7afed0e01ba9323df2801a0922dc1bf3a44dc3f
maihannn <maicapalillo15@gmail.com>
2024-01-26 20:52:40 +0800

    done, it worked

---
+++ downward_star_pattern.py
@@ -0,0 +1,12 @@
+# Name: Jamaica C. Palillo
+# Section: BSCPE 1-5
+
+# Exercise 14: Print a downward Half-Pyramid Pattern of Star (asterisk)
+
+
+for i in range (6, 0, -1):
+    for j in range (0, i -1):
+        print ("*", end= " ")
+    print ()
+    
+
